4447a52464b30ae3790312a0557f3fff905ce106
zXAZURIANXz <zxazurianxz@MacBook-Pro-de-zXAZURIANXz.local>
2025-07-07 17:07:33 -0600

    adding docker config·

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+	server: {
+    allowedHosts: [
+      '3bd5-189-188-163-182.ngrok-free.app' // Agrega aquí el host de ngrok
+    ]
+  },
   optimizeDeps: {
     exclude: ['flowbite-react']
   }
